4ce2a1573e5802f80f52f6e2503e43968e818407
brody <youoper@gmail.com>
2021-02-09 20:24:53 +0900

    leetcode problem #721 solution added

--- leetcode/721.cpp
+++ leetcode/721.cpp
@@ -79,3 +79,49 @@ public:
         return ret;
     }
 };
+
+
+/* make graph & dfs
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        unordered_map<string, vector<string>> graph;
+        for(int i = 0 ; i < accounts.size() ; ++i) {
+            for(int j = 1; j < accounts[i].size(); ++j) {
+                if(j > 1) {
+                    graph[accounts[i][j]].push_back(accounts[i][j-1]);
+                    graph[accounts[i][j-1]].push_back(accounts[i][j]);
+                }
+            }
+        }
+        
+        unordered_set<string> visited;
+        vector<vector<string>> ret;
+        for(int i = 0 ; i < accounts.size(); ++i) {
+            if(visited.find(accounts[i][1]) != visited.end()) {
+                continue;
+            }
+            ret.push_back({accounts[i][0]});
+            dfs(accounts[i][1], graph, visited, ret);
+        }
+        
+        for(int i = 0 ; i < ret.size(); ++i) {
+            sort(ret[i].begin() + 1, ret[i].end());
+        }
+        
+        return ret;
+    }
+    
+    void dfs(string& email, unordered_map<string, vector<string>>& graph, unordered_set<string>& visited, vector<vector<string>>& ret) {
+        visited.insert(email);
+        ret.back().push_back(email);
+        vector<string>& connected = graph[email];
+        for(int i = 0 ; i < connected.size() ; ++i ){
+            if(visited.find(connected[i]) == visited.end()) {
+                dfs(connected[i], graph, visited, ret);            
+            }
+        }
+        return;
+    }
+};
+*/
